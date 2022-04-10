c8f510b3607d74b6f7447c43f6f1ea5371a8de9d
Aditi Patra <Aditi.Patra@ibm.com>
2022-04-10 21:14:57 +0530

    Added components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import mealsImg from '../../assets/meals.jpeg'
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+return (<React.Fragment>
+    <header className={classes.header}>
+        <h1>Zoomers Food Delivery</h1>
+        <HeaderCartButton />
+    </header>
+    <div className={classes['main-image']}>
+        <img src={mealsImg} alt="Meal"/>
+    </div>
+</React.Fragment>);
+}
+
+export default Header;
